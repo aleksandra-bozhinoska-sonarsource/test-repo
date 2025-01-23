e9cf423ef739b3ff7580a0da6d6ef44b27eff6d9
Aleksandra Bozhinoska <aleksandra.bozhinoska@sonarsource.com>
2025-01-23 15:20:57 +0100

    Initial commit

---
+++ module1/src/main/java/org/example/App1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1
+{
+    public static void main( String[] args )
+    {
+        String out = "This should create an issue.";
+        System.out.println( "Hello World!" );
+    }
+}
