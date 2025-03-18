2dad8a237bdf2d29a18e931e422e92274935c3be
Aleksandra Bozhinoska <aleksandra.bozhinoska@sonarsource.com>
2025-03-18 14:15:50 +0100

    Add new issue

--- module1/src/main/java/org/example/App1.java
+++ module1/src/main/java/org/example/App1.java
@@ -9,6 +9,8 @@ public class App1
     public static void main( String[] args )
     {
         String out = "This should create an issue.";
+
         System.out.println( "Hello World!" );
+        String newIssue = "This should create another issue.";
     }
 }
